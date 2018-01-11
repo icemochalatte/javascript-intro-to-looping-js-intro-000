cf1b7c0004d13a6ced57f851f8fc89bc359efd8f
Joe Demeo <puredrag0n@yahoo.com>
2018-01-11 21:13:14 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,14 +1,12 @@
-forLoop ();
-
 function forLoop(array) {
 
   for(var i = 0; i < 25; i++) {
 
-if i = 1 {
+if (i === 1) {
     console.log( "I am 1 strange loop." );
 }
 else {
-    console.log( "I am ${i} strange loops." );
+    console.log( `I am ${i} strange loops.` );
 }
 }
 }
